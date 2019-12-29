230f1f0af0ba660a5b63fa747eddd6a18fefe99e
Lucasdfg07 <lucassiqueira07@hotmail.com>
2019-12-29 19:01:11 -0300

    Atualizações simultâneas da conversão

--- spec/system/pages_index_system_spec.rb
+++ spec/system/pages_index_system_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Exchange Currency Process", :type => :system, js: true do
       select('USD', from: 'target_currency')
       fill_in 'amount', with: rand(1..9999)
     end
-    click_button 'CONVERTER'
+    # click_button 'CONVERTER'
 
     # save_and_open_page
     expect(page).to have_content("value")
