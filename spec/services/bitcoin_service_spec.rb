960fa3be275879ca67368aa4243a25592536ac10
Lucasdfg07 <lucassiqueira07@hotmail.com>
2020-01-01 16:24:56 -0300

    Atualizações simultâneas da conversão

--- spec/services/bitcoin_service_spec.rb
+++ spec/services/bitcoin_service_spec.rb
@@ -6,7 +6,7 @@ describe 'Currency' do
   it 'exchange' do
     amount = rand(0..9999)
     res = BitcoinService.new("BRL", amount).perform
-    expect(res.is_a? Numeric).to eql(true)
+    expect(res.is_a? String).to eql(false)
     expect(res != 0 || amount == 0).to eql(true)
   end
 end
